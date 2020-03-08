282aa2a8e16a4fee754e828fe1075ff9c57f9b78
aartikansal <aartilumb@gmail.com>
2020-03-08 10:04:51 -0400

    cat test finished

--- src/test/java/io/zipcoder/polymorphism/CatTest.java
+++ src/test/java/io/zipcoder/polymorphism/CatTest.java
@@ -1,26 +1,57 @@
 package io.zipcoder.polymorphism;
 
 
-/*import org.junit.Test;
-
-public class CatTest {
+import org.junit.Assert;
+import org.junit.Test;
 
     public class CatTest {
 
         @Test
-        public void speakTest() {
+        public void testInheritance() {
+            Pet p = new Cat(null);
+            Assert.assertTrue(p instanceof Pet);
+        }
+
+        @Test
+        public void constructorWithNameTest() {
             // Given
-            Pet cat = new Cat();
+            String expectedName = "Name of Cat";
+            Cat cat = new Cat(expectedName);
 
             // When
-            String actualSpeak = cat.speak();
             String actualName = cat.getName();
 
-            // Assert
-            String expectedSpeak = "meow";
-            //Assert.assertEquals(expectedSpeak, actualSpeak);
-            String expectedName = "Default Name";
-            //Assert.assertEquals(expectedName, actualName);
+            // Then
+            Assert.assertEquals(expectedName, actualName);
         }
+
+        @Test
+        public void catSpeakTest(){
+            Cat cat = new Cat(null);
+            String expected = "You may feed me now.";
+            String actual = cat.speak();
+            Assert.assertEquals(expected,actual);
+        }
+
+        @Test
+        public void catGetNameTest(){
+            String expected = "Snowball";
+            Cat cat = new Cat(expected);
+            String actual = cat.getName();
+            Assert.assertEquals(expected,actual);
+        }
+
+        @Test
+        public void catSetNameTest(){
+            Cat cat = new Cat(null);
+            String expected = "Snowball";
+            cat.setName(expected);
+            String actual = cat.getName();
+            Assert.assertEquals(expected,actual);
+        }
+
     }
-}*/
+
+
+
+
